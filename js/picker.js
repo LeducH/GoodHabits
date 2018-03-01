68dca18d533fbd4e0ffcfa444613bf3c06cfadc7
huy le duc <huyleduc@zedat.fu-beerlin.de>
2018-03-01 14:45:13 +0100

    first commit

---
+++ js/picker.js
@@ -0,0 +1,73 @@
+var habits = [
+    "Be kind",
+    "Keep an open-mind",
+    "Eat well",
+    "Put your needs first",
+    "Exercise",
+    "Don't make excuses",
+    "Meditate",
+    "Speak well of others",
+    "Be honest",
+    "Listen to understand",
+    "Dream big",
+    "Choose faith over fear",
+    "Be patient",
+    "Make the most of now",
+    "Judge less",
+    "Exercise self-discipline",
+    "Smile often",
+    "Look on the bright side",
+    "Love yourself",
+    "Avoid social comparison",
+    "Forgive easily",
+    "See failure as opportunity",
+    "Show gratitude",
+    "Don't take opinions to heart",
+    "Think positively",
+    "Select friends that lift you up",
+    "Drink lots of water",
+    "Let go of what can't be changed",
+    "Believe in yourself",
+    "Have a healthy sleeping pattern"
+];
+
+function getRandom(arr, n) {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+}
+
+function hasOneDayPassed() {
+    var date = new Date().toLocaleDateString();
+
+    if (localStorage.yourapp_date == date)
+        return false;
+
+    localStorage.yourapp_date = date;
+    return true;
+}
+
+// some function which should run once a day
+function runOncePerDay() {
+    if (!hasOneDayPassed()) return false;
+
+    // your code below
+    var rand = getRandom(habits, 3);
+}
+runOncePerDay();
+var htmlHabits = [
+    "habit1",
+    "habit2",
+    "habit3"
+]
+for (var i = 0; i < htmlHabits.length; i++) {
+    document.getElementById(htmlHabits[i]).innerHTML = rand[i];
+}
