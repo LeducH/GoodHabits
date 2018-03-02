d835e059dd31bb77407baa65ec3823d61c42f666
huy le duc <huyleduc@zedat.fu-beerlin.de>
2018-03-02 19:48:08 +0100

    refactor

--- js/picker.js
+++ js/picker.js
@@ -3,7 +3,7 @@ function getRandom(arr, n) {
         len = arr.length,
         taken = new Array(len);
     if (n > len)
-        throw new RangeError("getRandom: more  elements taken than available");
+        throw new RangeError("getRandom: more elements taken than available");
     while (n--) {
         var x = Math.floor(Math.random() * len);
         result[n] = arr[x in taken ? taken[x] : x];
